bb868b89f12389eb933e3a0dfd93e31764d3484d
Himanshu Joshi <77668773+dishainnovation@users.noreply.github.com>
2025-03-01 18:10:36 +0530

    Update site_view.py

--- backend/attendance_tracker/app/site_view.py
+++ backend/attendance_tracker/app/site_view.py
@@ -9,6 +9,13 @@ class SiteViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = Site.objects.all().order_by('name')
         port_id = self.request.query_params.get('port_id', None)
+        site_id = self.request.query_params.get('id', None)
+        if site_id:
+            try:
+                site_id = int(site_id)
+                queryset = queryset.filter(id=site_id)
+            except ValueError:
+                pass
         if port_id:
             try:
                 port_id = int(port_id)
